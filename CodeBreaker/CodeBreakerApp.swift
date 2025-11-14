9f3b6a6f2104d3416d8e617ee956da3c172a5e64
Joel Lacerda <joellacerdaol@gmail.com>
2025-11-14 11:47:13 -0300

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Joel Lacerda on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by Joel Lacerda on 14/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
