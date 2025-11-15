eda7d67b7076f9302f23c98fc3dcca7a3a346882
Joel Lacerda <joellacerdaol@gmail.com>
2025-11-15 11:19:37 -0300

    ✨ feat(MatchMarkers): Implement the MatchMarkers view
    - Created the MatchMarkers view to display the results of a guess in the CodeBreaker game.
    - Added a placeholder Text view within MatchMarkers for initial setup.

---
+++ CodeBreaker/MatchMarkers.swift
@@ -0,0 +1,18 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by Joel Lacerda on 14/11/25.
+//
+
+import SwiftUI
+
+struct MatchMarkers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchMarkers()
+}
